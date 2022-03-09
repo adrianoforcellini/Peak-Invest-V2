9a3610c7a9e19a1f1a2a8f10a180f5b98ed19777
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2022-03-09 00:37:24 -0300

    Criada Logica simples que pode receber o numero de parcelas e retornar o valor apos o calculo ( Maximo de 999 parcelas ) -> verificar possibilidade de criar uma logica menos confusa

--- back-end/Controllers/WeatherForecastController.cs
+++ back-end/Controllers/WeatherForecastController.cs
@@ -6,13 +6,20 @@ namespace back_end.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-  public double Post([FromBody] string value)
+  public double Post([FromBody] string str)
   {
-    string input = value;
-    int num = Int32.Parse(input);
-    double add = num + (num * 0.15);
-    // System.Console.WriteLine(add);
+    string input = str;
+    string installments = input.Substring(0, 3);
+    string value = input.Substring(3);
 
-    return add;
+    int n1 = Int32.Parse(installments);
+    double n2 = Convert.ToDouble(value);
+
+    double multiplyValues = n1 * n2;
+    double addFifteenPercent = (multiplyValues * 0.15) + multiplyValues;
+
+    System.Console.WriteLine(addFifteenPercent);
+
+    return addFifteenPercent;
   }
 }
