93f6c7a9080c848c6479ba4285f56bc737a2a57f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2022-03-12 17:59:52 -0300

    Formulario Reativo

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
 
-  Console() {
-    console.log('oi')
+export class AppComponent {
+
+  public calculationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
+
+  createForm() {
+    this.calculationForm = this.fb.group({
+      installments: ['', Validators.required],
+      value: ['', Validators.required]
+    });
+  }
+
+  calculationSubmit() {
+    console.log(this.calculationForm.value)
+  }
+
 }
+
+    // async getByKey(key) {
+    //   await fetch(`https://localhost:7042/weatherforecast/${key}`)
+    //     .then(response => response.text())
+    //     .then(result => result)
+    // }
+  // }
+
