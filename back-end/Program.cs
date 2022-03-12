9de18b1de95205ba13390e25f2caec028be3d47f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2022-03-12 19:07:59 -0300

    Configuraçao do Cors -> agora API funciona para os fetchs

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -12,8 +12,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseCors();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.Run();
